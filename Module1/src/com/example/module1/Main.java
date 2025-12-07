9cb0aee7f3093404b6958948d8f7871ff4e7c37b
user <user@192.168.1.158>
2025-12-07 11:25:54 -0800

    First commit...

---
+++ Module1/src/com/example/module1/Main.java
@@ -0,0 +1,22 @@
+package com.example.module1;
+
+public class Main {
+
+
+	public static void main(String[] args) {
+	
+        String fName = "Brad";
+        String lName = "Pitted";
+        String address = "555 Hollywood ave.";
+        String city = "Los Angeles";
+        String zipCode = "90210";
+
+        System.out.println("First name: " + fName);
+        System.out.println("Last name: " + lName);
+        System.out.println("Street address: " + address);
+        System.out.println("City: " + city);
+        System.out.println("Zip code: " + zipCode);
+
+	}
+
+}
